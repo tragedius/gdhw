5fdd5dc09a3d1d34e113675d072b12cdb93b7125
root <root@gdhw.traged.net>
2017-09-19 09:48:14 +0200

    redesign :-)

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,26 @@ node default {
 	httpd::vhost { 'a.traged.net':
 		port 		=> 80,
 		server_name 	=> 'a.traged.net',
-		vhost_name 	=> '*',
+		vhost_name	=> "*",	
 		doc_root 	=> 'a.traged.net',
 	}
+
+	httpd::vhost_dummy { 'b.traged.net':
+		port		=> 80,
+		server_name     => 'b.traged.net',
+                vhost_name      => "*",
+	}
+
+	httpd::vhost_ssl { 'b.traged.net':
+		port 		=> 443,
+		server_name 	=> 'b.traged.net',
+		vhost_name	=> "*",	
+		doc_root 	=> 'b.traged.net',
+	}
+
+
+        file { "/etc/httpd/conf.d/welcome.conf":
+                ensure  => absent,
+	}
+
 }
--- modules/httpd/manifests/vhost.pp
+++ modules/httpd/manifests/vhost.pp
@@ -1,13 +1,27 @@
-define httpd::vhost ($port, $server_name, $vhost_name, $doc_root) {
+define httpd::vhost ($port, $server_name, $vhost_name, $ssl, $doc_root) {
 	require httpd
 	file { "/etc/httpd/conf.d/$server_name.conf":
 		content	=> template('/etc/puppet/modules/httpd/files/etc/httpd/conf.d/vhost.conf.erb'),
 		owner	=> 'root',
 		group 	=> 'root',
 		mode	=> '0644',
-	}
-	file { "/etc/httpd/conf.d/welcome.conf":
-		ensure	=> absent,
-	}
+		
+	if $ssl == true {
 	
+		file { "/etc/httpd/ssl/$server_name.crt":
+			ensure  => present,
+			source	=> "/etc/puppet/modules/httpd/files/etc/httpd/ssl/$server_name.crt",
+			owner	=> 'apache',
+			group 	=> 'apache',
+			mode	=> '0500',
+		}
+
+		file { "/etc/httpd/ssl/$server_name.key":
+			ensure  => present,
+			source	=> "/etc/puppet/modules/httpd/files/etc/httpd/ssl/$server_name.key",
+			owner	=> 'apache',
+			group 	=> 'apache',
+			mode	=> '0500',
+		}
+	}
 }
